e244a00d284c36584262b7aa7606cb30d81655b5
Misa <58565358+Misael-Venegas@users.noreply.github.com>
2024-05-09 08:53:16 -0600

    fisrt commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import '../output.css'
+import { NextUIProvider } from "@nextui-org/react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body >
+        <NextUIProvider>
+          {children}
+        </NextUIProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@nextui-org/react'
+import  Login  from '../components/login/Login'
+export default function Home() {
+  return (
+    <main >
+      <div className="card flex justify-content-center">
+        <Login />
+      </div>
+    </main>
+  );
+}

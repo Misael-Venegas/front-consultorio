f147f5d8caafc0325e7c18aebc162f2cf6a690b3
Misa <58565358+Misael-Venegas@users.noreply.github.com>
2024-05-30 22:36:57 -0600

    peticiones

---
+++ src/components/globals/ErrorAlert.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorAlert = ({ mensaje }) => {
+  return (
+    <div className="p-4 mt-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+      <span className="font-medium">Error!</span> {mensaje}
+    </div>
+  )
+}
+
+export default ErrorAlert
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,21 +1,53 @@
 "use client";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Avatar, Input, Button } from '@nextui-org/react'
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import ErrorAlert from '../globals/ErrorAlert';
 
 
 const Login = () => {
+    console.log('url', process.env.NEXT_PUBLIC_API_URL)
     const [isVisible, setIsVisible] = React.useState(false);
-
+    const [usuario, setusuario] = useState('')
+    const [contrasenha, setcontrasenha] = useState('')
+    const [mesgError, setMesgError] = useState('')
     const toggleVisibility = () => setIsVisible(!isVisible);
+
+    const logIn = async () => {
+        setMesgError('')
+        if (usuario == '' || contrasenha == '') {
+            setMesgError('Verifica que los campos usuario y/o contraseña no estén vacíos')
+            return
+        }
+        const url = process.env.NEXT_PUBLIC_API_URL
+        try {
+            const response = await fetch(`${url}users/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    usuario: usuario,
+                    contrasena: contrasenha,
+                })
+            })
+
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            setMesgError(error.message)
+        }
+    }
 
     return (
         <>
             <div className='principal-div-login' >
                 <div className='div-secundario-login' >
                     <p className='mb-5 tituloLogin' >Eyeconic mx</p>
-                    <img src="/assets/Images/Eyeconic_2.PNG" width={250} alt="" />  
-                    <Input label='Usuario' className='mb-5' />
+                    <img src="/assets/Images/Eyeconic_2.PNG" width={250} alt="" />
+                    <Input label='Usuario' className='mb-5'
+                        onChange={(e) => setusuario(e.target.value)}
+                    />
                     <Input
                         label='Contraseña'
                         className='mb-5'
@@ -30,9 +62,13 @@ const Login = () => {
                             </button>
                         }
                         type={isVisible ? "text" : "password"}
+                        onChange={(e) => setcontrasenha(e.target.value)}
 
                     />
-                    <Button color='primary' style={{ width: '100%' }} >  Iniciar Sesión</Button>
+                    <Button color='primary' style={{ width: '100%' }} onClick={() => logIn()} >  Iniciar Sesión</Button>
+                    {
+                        mesgError != '' && <ErrorAlert mensaje={mesgError} />
+                    }
                 </div>
             </div>
         </>

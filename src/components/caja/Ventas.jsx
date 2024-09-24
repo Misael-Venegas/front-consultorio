cebcfc0f9a4e2a173cd66a514486563867e7565a
Misa <misa.cc2393293@gmail.com>
2024-09-23 22:42:46 -0600

    Vista ventas optica

--- src/components/caja/Ventas.jsx
+++ src/components/caja/Ventas.jsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Input } from '@nextui-org/react'
+import { Divider, Input } from '@nextui-org/react'
 
 import React from 'react'
 const Ventas = () => {
@@ -6,8 +6,32 @@ const Ventas = () => {
         <>
             <div className='flex flex-col md:flex-row'>
                 <div className='w-full md:w-[60%] mr-10 ml-10 ' >
+
                     <span>Nombre del producto / Código de barras</span>
                     <Input className='pt-3' placeholder='Ingrese el nombre del producto o el código de barras' />
+                    <Divider className='my-4' />
+
+                    <div className=' pt-3 flex flex-col md:flex-row'  >
+                        <div className='w-full md:w-[70%]' >
+                            <span>  Producto: Solucion salina</span>
+                        </div>
+                        <div className='w-full md:w-[30%]'>
+                            <span> Stock disponible:</span>
+                        </div>
+                    </div>
+
+                    <div className='pt-3 flex flex-col md:flex-row' >
+                        <div className='pt-3 w-full md:w[33.3%] ' >
+                            <span>Precio unitario:</span>
+                        </div>
+                        <div className='pt-3 w-full md:w[33.3%] ' >
+                            Cantidad:
+                        </div>
+                        <div className='pt-3 w-full md:w[33.3%] ' >
+                            <span>Descuento:</span>
+                        </div>
+                    </div>
+
                 </div>
                 <div className='w-full md:w-[40%] mr-10 ml-10' >
                     <span>Productos</span>

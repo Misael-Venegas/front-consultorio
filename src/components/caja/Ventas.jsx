11516c0b5eb01419832081ab1edabe880e098e71
Misa <misa.cc2393293@gmail.com>
2024-09-24 23:16:46 -0600

    codigo de barras

--- src/components/caja/Ventas.jsx
+++ src/components/caja/Ventas.jsx
@@ -1,36 +1,52 @@
-import { Divider, Input } from '@nextui-org/react'
+'use client'
+import { Divider, Input, Button, Autocomplete } from '@nextui-org/react'
 
-import React from 'react'
+import React, { useState } from 'react'
 const Ventas = () => {
+    const [producto, setProducto] = useState([])
+    const obtenerProductos = (e) => {
+        const value = e.target.value;
+        console.log('Se buscaran productos')
+    }
     return (
         <>
             <div className='flex flex-col md:flex-row'>
                 <div className='w-full md:w-[60%] mr-10 ml-10 ' >
 
                     <span>Nombre del producto / Código de barras</span>
-                    <Input className='pt-3' placeholder='Ingrese el nombre del producto o el código de barras' />
+
+                    <Autocomplete onChange={(e) => obtenerProductos(e)} className='pt-3' placeholder='Ingrese el nombre del producto o el código de barras' >
+                    </Autocomplete>
+
                     <Divider className='my-4' />
+                    {producto.length > 0 && <>
+                        <div className=' pt-3 flex flex-col md:flex-row'  >
+                            <div className='w-full md:w-[70%]' >
+                                <span>  Producto</span>
+                            </div>
+                            <div className='w-full md:w-[30%]'>
+                                <span> Stock disponible</span>
+                            </div>
+                        </div>
 
-                    <div className=' pt-3 flex flex-col md:flex-row'  >
-                        <div className='w-full md:w-[70%]' >
-                            <span>  Producto: Solucion salina</span>
+                        <div className='pt-3 flex flex-col md:flex-row' >
+                            <div className='pt-3 w-full md:w[33.3%] ' >
+                                <span>Precio unitario</span>
+                            </div>
+                            <div className='pt-3 w-full md:w[33.3%] ' >
+                                Cantidad
+                            </div>
+                            <div className='pt-3 w-full md:w[33.3%] ' >
+                                <span>Descuento</span>
+                            </div>
                         </div>
-                        <div className='w-full md:w-[30%]'>
-                            <span> Stock disponible:</span>
+                        <div className='float-none pt-7' >
+                            <Button color='primary' size='md'>
+                                Agregar a la lista
+                            </Button>
                         </div>
-                    </div>
-
-                    <div className='pt-3 flex flex-col md:flex-row' >
-                        <div className='pt-3 w-full md:w[33.3%] ' >
-                            <span>Precio unitario:</span>
-                        </div>
-                        <div className='pt-3 w-full md:w[33.3%] ' >
-                            Cantidad:
-                        </div>
-                        <div className='pt-3 w-full md:w[33.3%] ' >
-                            <span>Descuento:</span>
-                        </div>
-                    </div>
+                    </>
+                    }
 
                 </div>
                 <div className='w-full md:w-[40%] mr-10 ml-10' >

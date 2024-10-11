0ce16634be294d1c6b1d1f997a69192789b55a06
Misa <misa.cc2393293@gmail.com>
2024-10-10 21:13:11 -0600

    productros stock minimo

--- src/components/incio/TablaProductosCantidadMinima.jsx
+++ src/components/incio/TablaProductosCantidadMinima.jsx
@@ -1,36 +1,83 @@
-import React from 'react'
-import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from '@nextui-org/react'
+import React, { useEffect, useState } from 'react'
+import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell, Spinner, Chip } from '@nextui-org/react'
+
+import { useNotification } from '@/helpers/NotificationContext'
+
 const TablaProductosCantidadMinima = () => {
+    const { showNotification } = useNotification()
+    const [stock, setStock] = useState([])
+    const [loading, setloading] = useState(false)
+
+    useEffect(() => {
+        obtenerStockMinimo()
+    }, [])
+
+    const obtenerStockMinimo = async () => {
+        try {
+            setloading(true)
+            const urlAPI = process.env.NEXT_PUBLIC_API_URL
+            const token = sessionStorage.getItem('token')
+            const response = await fetch(`${urlAPI}obtener-stock-minimo`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                }
+            })
+
+            if (!response.ok) {
+                const dataError = await response.json()
+                throw new Error(dataError.message)
+            }
+            const data = await response.json()
+            setStock(data)
+        } catch (error) {
+            showNotification(error.message, 'error')
+        } finally {
+            setloading(false)
+        }
+    }
+
     return (
         <div className='my-8' >
             <h4 className='font-bold text-large' >Productos con stock bajo </h4>
-            <Table className='pr-3' >
-                <TableHeader>
-                    <TableColumn>#</TableColumn>
-                    <TableColumn>Nombre</TableColumn>
-                    <TableColumn>Código de barras</TableColumn>
-                    <TableColumn>Cantidad </TableColumn>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>
 
-                        </TableCell>
+            {
+                loading && <Spinner />
+            }
+            {
+                !loading && <Table className='pr-3' >
+                    <TableHeader>
+                        <TableColumn>#</TableColumn>
+                        <TableColumn>Nombre</TableColumn>
+                        <TableColumn>Código de barras</TableColumn>
+                        <TableColumn>Cantidad </TableColumn>
+                    </TableHeader>
+                    <TableBody emptyContent={"No hay datos disponibles"} >
+                        {
+                            stock.map((data, key) => {
+                                return <TableRow key={key} >
+                                    <TableCell>
+                                        {key + 1}
+                                    </TableCell>
 
-                        <TableCell>
+                                    <TableCell>
+                                        {data.nombre_producto}
+                                    </TableCell>
 
-                        </TableCell>
+                                    <TableCell>
+                                        {data.codigo_barras}
+                                    </TableCell>
 
-                        <TableCell>
-
-                        </TableCell>
-
-                        <TableCell>
-
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
+                                    <TableCell>
+                                        {data.cantidad > 0 ? <Chip color="warning" variant="bordered"> {data.cantidad} </Chip> : <Chip color="danger" variant="bordered"> {data.cantidad} </Chip>}
+                                    </TableCell>
+                                </TableRow>
+                            })
+                        }
+                    </TableBody>
+                </Table>
+            }
         </div>
     )
 }
